02469842ee1d5a97dd630afb2eddb176d6c84eaa
Brian Peiris <brianpeiris@gmail.com>
2016-09-21 19:16:07 -0700

    update component

--- dist/aframe-altspace-component.js
+++ dist/aframe-altspace-component.js
@@ -215,21 +215,24 @@
 	    altspace.getGamepads();
 	  },
 	  tick: function () {
-	      if (this.trackedControlsSystem && this.systemGamepads !== this.trackedControlsSystem.controllers.length && window.altspace && altspace.getGamepads && altspace.getGamepads().length) {
-	        if (this.el.components['paint-controls']) {
-	          this.gamepadIndex = this.el.components['paint-controls'].data.hand === 'left' ? 2 : 1;
+	      if (
+	        this.trackedControlsSystem &&
+	        this.systemGamepads !== this.trackedControlsSystem.controllers.length &&
+	        window.altspace && altspace.getGamepads && altspace.getGamepads().length
+	      ) {
+	        var components = this.el.components;
+	        if (components['paint-controls']) {
+	          this.gamepadIndex = components['paint-controls'].data.hand === 'left' ? 2 : 1;
 	        }
-	        if (this.gamepadIndex === null && this.el.components['hand-controls']) {
-	          this.gamepadIndex = this.el.components['hand-controls'].data === 'left' ? 2 : 1;
+	        if (this.gamepadIndex === null && components['hand-controls']) {
+	          this.gamepadIndex = components['hand-controls'].data === 'left' ? 2 : 1;
 	        }
-	        if (this.gamepadIndex === null && this.el.components['vive-controls']) {
-	          this.gamepadIndex = this.el.components['vive-controls'].data.hand === 'left' ? 2 : 1;
+	        if (this.gamepadIndex === null && components['vive-controls']) {
+	          this.gamepadIndex = components['vive-controls'].data.hand === 'left' ? 2 : 1;
 	        }
-	        if (this.gamepadIndex === null && this.el.components['tracked-controls']) {
-	          this.gamepadIndex = this.el.components['tracked-controls'].data.controller;
+	        if (this.gamepadIndex === null && components['tracked-controls']) {
+	          this.gamepadIndex = components['tracked-controls'].data.controller;
 	        }
-	        console.log('BPDEBUG gamepadIndex', this.gamepadIndex);
-	        console.log('BPDEBUG id', altspace.getGamepads()[this.gamepadIndex].id);
 	        this.el.setAttribute('tracked-controls', 'id', altspace.getGamepads()[this.gamepadIndex].id);
 	        this.el.setAttribute('tracked-controls', 'controller', 0);
 	        this.systemGamepads = this.trackedControlsSystem.controllers.length;
